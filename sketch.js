8b45265d77ced864b54a5c14186a28ffb0f0f959
dorahur931-glitch <dorahur931@gmail.com>
2026-06-01 01:14:15 +0900

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,327 @@
+// =====================================
+// REALTIME COLOR HARMONY FIELD
+// p5.js + p5.sound
+//
+// index.html 에 추가:
+// <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.9.0/p5.min.js"></script>
+// <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.9.0/addons/p5.sound.min.js"></script>
+//
+// helvetica.ttf 파일을 sketch.js와 같은 폴더에 위치시킬 것
+//
+// =====================================
+
+let cam;
+let camLayer;
+let uiLayer;
+
+let soundPlayers = [];
+let started = false;
+let reverb;
+
+// 파티클 배열
+let particles = [];
+let accumulatedParticles = []; // 바닥에 쌓인 파티클들
+
+// 폰트
+let helveticaFont;
+
+// C Major 7음계와 색상 매핑
+const colorNotes = [
+  { name: 'red', freq: 261.63, hue: [345, 15], rgb: [255, 50, 50], label: 'c' },
+  { name: 'orange', freq: 293.66, hue: [15, 45], rgb: [255, 165, 50], label: 'd' },
+  { name: 'yellow', freq: 329.63, hue: [45, 70], rgb: [255, 255, 50], label: 'e' },
+  { name: 'green', freq: 349.23, hue: [70, 160], rgb: [50, 255, 50], label: 'f' },
+  { name: 'cyan', freq: 392.00, hue: [160, 200], rgb: [50, 255, 255], label: 'g' },
+  { name: 'blue', freq: 440.00, hue: [200, 260], rgb: [50, 100, 255], label: 'a' },
+  { name: 'purple', freq: 493.88, hue: [260, 345], rgb: [200, 50, 255], label: 'b' }
+];
+
+// 음표 모양 위치 (왼쪽/오른쪽)
+const noteShapeLeft = [
+  { x: 0.1, y: 0.5 },   // 음표 머리 (왼쪽 하단)
+  { x: 0.12, y: 0.3 },  // 음표 기둥 중간
+  { x: 0.14, y: 0.15 }, // 음표 기둥 상단
+];
+
+const noteShapeRight = [
+  { x: 0.9, y: 0.5 },   // 음표 머리 (오른쪽 하단)
+  { x: 0.88, y: 0.3 },  // 음표 기둥 중간
+  { x: 0.86, y: 0.15 }, // 음표 기둥 상단
+];
+
+// 파티클 클래스
+class ColorParticle {
+  constructor(colorData, intensity, side) {
+    this.side = side; // 'left' or 'right'
+    
+    // 양 옆에서만 생성
+    if (side === 'left') {
+      this.x = random(0, width * 0.2); // 왼쪽 20% 영역
+    } else {
+      this.x = random(width * 0.8, width); // 오른쪽 20% 영역
+    }
+    
+    this.y = random(-100, -20);
+    this.speed = map(intensity, 0, 1, 4, 12);
+    this.color = colorData.rgb;
+    this.label = colorData.label;
+    this.size = map(intensity, 0, 1, 10, 22.5);
+    this.alpha = 255;
+    this.rotation = random(-0.2, 0.2);
+    this.rotSpeed = random(-0.05, 0.05);
+    this.bouncing = false;
+    this.bounceVelocity = 0;
+    this.bounceY = 0;
+    this.settled = false;
+    this.targetX = 0;
+    this.targetY = 0;
+  }
+  
+  update() {
+    if (this.settled) {
+      // 이미 쌓인 상태면 고정
+      return;
+    }
+    
+    if (!this.bouncing) {
+      this.y += this.speed;
+      this.rotation += this.rotSpeed;
+      
+      // 바닥에 닿으면 바운스 시작
+      if (this.y >= height - 50) {
+        this.bouncing = true;
+        this.bounceY = height - 50;
+        this.bounceVelocity = -6;
+      }
+    } else {
+      // 바운스 물리
+      this.bounceVelocity += 0.6;
+      this.bounceY += this.bounceVelocity;
+      this.y = this.bounceY;
+      
+      // 바운스 후 음표 모양으로 이동
+      if (this.bounceVelocity > 0 && this.y >= height - 60) {
+        this.settleIntoNote();
+      }
+    }
+  }
+  
+  settleIntoNote() {
+    this.settled = true;
+    
+    // 음표 모양 위치 선택
+    let noteShape = this.side === 'left' ? noteShapeLeft : noteShapeRight;
+    let targetPos = random(noteShape);
+    
+    // 랜덤 오프셋으로 자연스러운 쌓임
+    this.targetX = targetPos.x * width + random(-30, 30);
+    this.targetY = targetPos.y * height + random(-20, 20);
+    
+    this.x = this.targetX;
+    this.y = this.targetY;
+    this.rotation = 0;
+    
+    accumulatedParticles.push(this);
+  }
+  
+  display() {
+    push();
+    translate(this.x, this.y);
+    rotate(this.rotation);
+    
+    fill(this.color[0], this.color[1], this.color[2], this.alpha);
+    textAlign(CENTER, CENTER);
+    textSize(this.size);
+    textFont(helveticaFont);
+    text(this.label, 0, 0);
+    
+    pop();
+  }
+  
+  isDead() {
+    return this.settled;
+  }
+}
+
+function preload() {
+  helveticaFont = loadFont('helvetica.ttf');
+}
+
+function setup() {
+  createCanvas(960, 540);
+  
+  camLayer = createGraphics(960, 540);
+  uiLayer = createGraphics(960, 540);
+  
+  cam = createCapture(VIDEO);
+  cam.size(160, 120);
+  cam.hide();
+  
+  textAlign(CENTER, CENTER);
+}
+
+function mousePressed() {
+  if (!started) {
+    userStartAudio();
+    
+    reverb = new p5.Reverb();
+    reverb.set(3, 2);
+    
+    for (let i = 0; i < colorNotes.length; i++) {
+      let osc = new p5.Oscillator();
+      osc.setType('triangle');
+      osc.freq(colorNotes[i].freq);
+      osc.amp(0);
+      osc.start();
+      
+      reverb.process(osc, 3, 2);
+      
+      soundPlayers.push({
+        colorData: colorNotes[i],
+        osc: osc,
+        currentAmp: 0
+      });
+    }
+    
+    started = true;
+  }
+}
+
+function draw() {
+  background(0);
+  
+  camLayer.clear();
+  camLayer.push();
+  camLayer.translate(camLayer.width, 0);
+  camLayer.scale(-1, 1);
+  camLayer.image(cam, 0, 0, camLayer.width, camLayer.height);
+  camLayer.pop();
+  
+  image(camLayer, 0, 0);
+  
+  if (!started) {
+    fill(255);
+    textSize(48);
+    textStyle(BOLD);
+    text("CLICK TO START", width / 2, height / 2);
+    return;
+  }
+  
+  analyzeAndPlayHarmony();
+  updateParticles();
+  
+  image(uiLayer, 0, 0);
+}
+
+function analyzeAndPlayHarmony() {
+  cam.loadPixels();
+  
+  if (cam.pixels.length < 10) return;
+  
+  let colorAmount = {};
+  for (let i = 0; i < colorNotes.length; i++) {
+    colorAmount[colorNotes[i].name] = 0;
+  }
+  
+  let total = 0;
+  
+  for (let i = 0; i < cam.pixels.length; i += 80) {
+    let r = cam.pixels[i];
+    let g = cam.pixels[i + 1];
+    let b = cam.pixels[i + 2];
+    
+    let h = rgbToHue(r, g, b);
+    
+    for (let j = 0; j < colorNotes.length; j++) {
+      let hueRange = colorNotes[j].hue;
+      if (j === 0) {
+        if (h >= hueRange[0] || h < hueRange[1]) {
+          colorAmount[colorNotes[j].name]++;
+        }
+      } else {
+        if (h >= hueRange[0] && h < hueRange[1]) {
+          colorAmount[colorNotes[j].name]++;
+        }
+      }
+    }
+    
+    total++;
+  }
+  
+  for (let i = 0; i < soundPlayers.length; i++) {
+    let colorName = soundPlayers[i].colorData.name;
+    let ratio = colorAmount[colorName] / total;
+    
+    let targetAmp = map(ratio, 0, 0.35, 0, 0.4, true);
+    soundPlayers[i].osc.amp(targetAmp, 0.2);
+    
+    if (ratio > 0.02) {
+      let particleCount = floor(map(ratio, 0.02, 0.35, 0.6, 2.4, true));
+      for (let p = 0; p < particleCount; p++) {
+        if (random() < 0.36) {
+          let intensity = map(ratio, 0.02, 0.35, 0.3, 1, true);
+          let side = random() < 0.5 ? 'left' : 'right'; // 양쪽에서 랜덤 생성
+          particles.push(new ColorParticle(soundPlayers[i].colorData, intensity, side));
+        }
+      }
+    }
+    
+    soundPlayers[i].currentAmp = targetAmp;
+  }
+}
+
+function updateParticles() {
+  uiLayer.clear();
+  
+  // 쌓인 파티클 먼저 표시
+  for (let i = 0; i < accumulatedParticles.length; i++) {
+    accumulatedParticles[i].display();
+  }
+  
+  // 떨어지는 파티클 업데이트 및 표시
+  for (let i = particles.length - 1; i >= 0; i--) {
+    particles[i].update();
+    
+    uiLayer.push();
+    uiLayer.translate(particles[i].x, particles[i].y);
+    uiLayer.rotate(particles[i].rotation);
+    
+    uiLayer.fill(particles[i].color[0], particles[i].color[1], particles[i].color[2], particles[i].alpha);
+    uiLayer.textAlign(CENTER, CENTER);
+    uiLayer.textSize(particles[i].size);
+    uiLayer.textFont(helveticaFont);
+    uiLayer.text(particles[i].label, 0, 0);
+    
+    uiLayer.pop();
+    
+    if (particles[i].isDead()) {
+      particles.splice(i, 1);
+    }
+  }
+}
+
+function rgbToHue(r, g, b) {
+  r /= 255;
+  g /= 255;
+  b /= 255;
+  
+  let maxVal = max(r, g, b);
+  let minVal = min(r, g, b);
+  let d = maxVal - minVal;
+  let h = 0;
+  
+  if (d === 0) {
+    h = 0;
+  } else if (maxVal === r) {
+    h = ((g - b) / d) % 6;
+  } else if (maxVal === g) {
+    h = (b - r) / d + 2;
+  } else {
+    h = (r - g) / d + 4;
+  }
+  
+  h *= 60;
+  if (h < 0) h += 360;
+  
+  return h;
+}
