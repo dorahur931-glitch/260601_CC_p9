4bc6221a6af69ff475b428539d3620e3f8a6195d
dorahur931-glitch <dorahur931@gmail.com>
2026-06-01 10:22:02 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -312,13 +312,30 @@ function draw() {
     text("CLICK OR TAP TO START", width / 2, height / 2);
     return;
   }
-  
+
+  drawNoteOutlines();
   analyzeAndPlayHarmony();
   updateParticles();
   
   image(uiLayer, 0, 0);
 }
-
+function drawNoteOutlines() {
+  stroke(100, 100, 100, 80);
+  strokeWeight(2);
+  noFill();
+  
+  ellipse(width * 0.05, height * 0.80, 40, 40);
+  ellipse(width * 0.12, height * 0.68, 40, 40);
+  line(width * 0.05, height * 0.42, width * 0.05, height * 0.80);
+  line(width * 0.12, height * 0.32, width * 0.12, height * 0.68);
+  line(width * 0.05, height * 0.42, width * 0.12, height * 0.32);
+  
+  ellipse(width * 0.95, height * 0.80, 40, 40);
+  ellipse(width * 0.88, height * 0.68, 40, 40);
+  line(width * 0.95, height * 0.42, width * 0.95, height * 0.80);
+  line(width * 0.88, height * 0.32, width * 0.88, height * 0.68);
+  line(width * 0.88, height * 0.32, width * 0.95, height * 0.42);
+}
 function analyzeAndPlayHarmony() {
   cam.loadPixels();
   
